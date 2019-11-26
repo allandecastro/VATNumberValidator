8f736575173613e1fb89b8fb210b9d15e2bcb47b
Allan De Castro <allan.de.castro@avanade.com>
2019-11-26 18:45:58 +0100

    Add Security + clean

--- VATNumberValidator/index.ts
+++ VATNumberValidator/index.ts
@@ -34,47 +34,28 @@ export class VATNumberValidator implements ComponentFramework.StandardControl<II
 		this._context = context;
 		this._container = container;
 		this._notifyOutputChanged = notifyOutputChanged;
-		//Get all parameters.
 		this._vatNumberChanged = this.vatNumberChanged.bind(this);
 
 		this._vatNumber = this._context.parameters.vatNumberfield == null || this._context.parameters.vatNumberfield.raw == null ? "" : this._context.parameters.vatNumberfield.raw;
 		this._companyName = this._context.parameters.companyName == null || this._context.parameters.companyName.raw == null ? "" : this._context.parameters.companyName.raw;
 		this._companyAddress = this._context.parameters.companyAddress == null || this._context.parameters.companyAddress.raw == null ? "" : this._context.parameters.companyAddress.raw;
 
-		// Adding the main table to the container DIV.
 		this._container = document.createElement("div");
 		container.appendChild(this._container);
-		//try to create style for color variable
 		this._vatNumberElement = document.createElement("input");
 		this._vatNumberElement.setAttribute("type", "text");
-		this._vatNumberElement.setAttribute("placeholder", "Insert a VAT Number..");
 		this._vatNumberElement.setAttribute("class", "pcfvatinputcontrol");
 		this._vatNumberElement.addEventListener("change", this._vatNumberChanged);
 		this._vatNumberTypeElement = document.createElement("img");
 		this._vatNumberTypeElement.setAttribute("class", "pcfvatimagecontrol");
 		this._vatNumberTypeElement.setAttribute("height", "24px");
-
 		this._container.appendChild(this._vatNumberElement);
 		this._container.appendChild(this._vatNumberTypeElement);
-
-		// @ts-ignore
-		// if (Xrm.Page.ui.getFormType() == 3 || Xrm.Page.ui.getFormType() == 4)
-		// 	this._vatNumberElement.readOnly = true;
-		this._vatNumber = this._context.parameters.vatNumberfield == null || this._context.parameters.vatNumberfield.raw == null ? "" : this._context.parameters.vatNumberfield.raw;
-		if (this._vatNumber != null && this._vatNumber.length > 0) {
-			this._vatNumberElement.value = this._vatNumber;
-			this.CheckVatNumber();
-			this._notifyOutputChanged();
-
-		}
 	}
-
-
 	/**
 	* Called when a change is detected in the phone number input.
 	*/
 	private vatNumberChanged(evt: Event): void {
-		//this.findAndSetFlag()
 		this.CheckVatNumber();
 		this._notifyOutputChanged();
 	}
@@ -84,18 +65,50 @@ export class VATNumberValidator implements ComponentFramework.StandardControl<II
 	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
 	 */
 	public updateView(context: ComponentFramework.Context<IInputs>): void {
-		// Add code to update control view
-		// textbox control
-		this._vatNumber = this._context.parameters.vatNumberfield == null || this._context.parameters.vatNumberfield.raw == null ? "" : this._context.parameters.vatNumberfield.raw;
+		let visible = this._context.mode.isVisible;
+		if (visible) {
+			this.SecurityEnablement(this._vatNumberElement);
+			this._vatNumber = this._context.parameters.vatNumberfield == null || this._context.parameters.vatNumberfield.raw == null ? "" : this._context.parameters.vatNumberfield.raw;
+			if (this._vatNumber != null && this._vatNumber.length > 0) {
+				this._vatNumberElement.value = this._vatNumber;
+				this.CheckVatNumber();
+				this._notifyOutputChanged();
+			}
+		}
+	}
+	/**
+	 * Used to implement the security model about the targeted field
+	 * @param _vatNumberElement The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+	 */
+	private SecurityEnablement(_vatNumberElement: HTMLInputElement): void {
+
+		let readOnly = this._context.mode.isControlDisabled; // If the form is diabled because it is inactive or the user doesn't have access isControlDisabled is set to true
+		// When a field has FLS enabled, the security property on the attribute parameter is set
+		let masked = false;
+		if (this._context.parameters.vatNumberfield.security) {
+			readOnly = readOnly || !this._context.parameters.vatNumberfield.security.editable;
+			masked = !this._context.parameters.vatNumberfield.security.readable;
+		}
+		if (masked)
+			this._vatNumberElement.setAttribute("placeholder", "*******");
+		else
+			this._vatNumberElement.setAttribute("placeholder", "Insert a VAT Number..");
+		if (readOnly)
+			this._vatNumberElement.readOnly = true;
+		else
+			this._vatNumberElement.readOnly = false;
 	}
 
-	public CheckVatNumber() {
+	/**
+	 * Used to query the SOAP services and set the result to the appropriate fields.
+	 */
+	private CheckVatNumber(): void {
 		this._vatNumber = this._vatNumberElement.value;
 		if (this._vatNumber.length > 0) {
 			var xmlhttp = new XMLHttpRequest();
-			xmlhttp.open('POST', 'https://cors-anywhere.herokuapp.com/https://euvat-cors-reverse-proxy.glitch.me/vies', false);
+			xmlhttp.open('POST', 'https://cors-anywhere.herokuapp.com/https://euvat-cors-reverse-proxy.glitch.me/vies', true);
 			// build SOAP request
-			var sr = "<?xml version='1.0' encoding='UTF-8'?>" +
+			var soadpRequest: string = "<?xml version='1.0' encoding='UTF-8'?>" +
 				"<SOAP-ENV:Envelope xmlns:ns0='urn:ec.europa.eu:taxud:vies:services:checkVat:types'" +
 				" xmlns:ns1='http://schemas.xmlsoap.org/soap/envelope/'" +
 				" xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'" +
@@ -109,8 +122,7 @@ export class VATNumberValidator implements ComponentFramework.StandardControl<II
 			xmlhttp.onreadystatechange = function () {
 				if (xmlhttp.readyState == 4) {
 					if (xmlhttp.status == 200) {
-						console.log(xmlhttp.responseText);
-						var parser, xmlDoc;
+						var parser: DOMParser, xmlDoc: any;
 						parser = new DOMParser();
 						xmlDoc = parser.parseFromString(xmlhttp.responseText, "text/xml");
 						if (xmlDoc.getElementsByTagName("valid")[0] != undefined) {
@@ -119,24 +131,23 @@ export class VATNumberValidator implements ComponentFramework.StandardControl<II
 								_this._companyName = xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue == null || xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue == undefined ? "" : <string>xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
 							if (xmlDoc.getElementsByTagName("address")[0] != undefined)
 								_this._companyAddress = xmlDoc.getElementsByTagName("address")[0].childNodes[0].nodeValue == null || xmlDoc.getElementsByTagName("address")[0].childNodes[0].nodeValue == undefined ? "" : <string>xmlDoc.getElementsByTagName("address")[0].childNodes[0].nodeValue;
-							//_this._vatNumberTypeElement.setAttribute("src", "/img/" + _this._vatNumber.slice(0, 2).toLowerCase() + ".png");
 							_this.findAndSetImage(_this._vatNumber.slice(0, 2).toLowerCase());
 						}
 						else {
 							_this._isValid = false;
-							//_this._vatNumberTypeElement.setAttribute("src", "/img/warning.png");					
-							_this._context.navigation.openAlertDialog({ text: "Aucun résultat trouvé poru le numéro suivant: " + _this._vatNumber });
+							_this._context.navigation.openAlertDialog({ text: "No result found for the following VAT Number: " + _this._vatNumber });
 							_this.findAndSetImage("warning");
 						}
 					}
-					else
+					else {
 						_this._context.navigation.openAlertDialog({ text: "Problème avec le service distant: " + xmlhttp.status });
+						_this.findAndSetImage(_this._vatNumber.slice(0, 2).toLowerCase());
+					}
 				}
 			}
 			// Send the POST request
 			xmlhttp.setRequestHeader('Content-Type', 'text/xml');
-			xmlhttp.send(sr);
-
+			xmlhttp.send(soadpRequest);
 		}
 	}
 	/** 
@@ -144,10 +155,6 @@ export class VATNumberValidator implements ComponentFramework.StandardControl<II
 	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
 	 */
 	public getOutputs(): IOutputs {
-		console.log("_isValid: " + this._isValid);
-		console.log(" _vatNumber: " + this._vatNumber);
-		console.log(" _companyName: " + this._companyName);
-		console.log(" _companyAddress: " + this._companyAddress);
 		return {
 			vatNumberfield: this._vatNumber,
 			isVatNumberValid: this._isValid,
@@ -170,12 +177,26 @@ export class VATNumberValidator implements ComponentFramework.StandardControl<II
 	private findAndSetImage(imageName: string) {
 		this._context.resources.getResource("img/" + imageName + ".png",
 			data => {
-				console.log('sucess');
 				this._vatNumberTypeElement.setAttribute("src", this.generateImageSrcUrl(".png", data));
 			},
 			() => {
 				console.log('Error when downloading ' + imageName + '.png image.');
 			});
+	}
+	/**
+ * * This function return a string converted in PascalCase format.
+ * @param stringToConvert: String variable to be converted in PascalCase format.
+ */
+	private ToPascalCase(stringToConvert: string): string {
+		return `${stringToConvert}`
+			.replace(new RegExp(/[-_]+/, 'g'), ' ')
+			.replace(new RegExp(/[^\w\s]/, 'g'), '')
+			.replace(
+				new RegExp(/\s+(.)(\w+)/, 'g'),
+				($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`
+			)
+			.replace(new RegExp(/\s/, 'g'), '')
+			.replace(new RegExp(/\w/), s => s.toUpperCase());
 	}
 	/**
  	* Called when a change is detected in the phone number input
